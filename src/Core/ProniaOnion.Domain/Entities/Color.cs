f5e23432a733c5e355512d0eb4221008a855f073
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-12-26 22:53:16 -0800

    Onion Architecture intro

---
+++ src/Core/ProniaOnion.Domain/Entities/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Domain.Entities
+{
+    public class Color:BaseNameableEntity
+    {
+
+        //Relational Properties
+       public ICollection<ProductColor> ProductColors { get; set; }
+
+    }
+}
---
+++ src/Core/ProniaOnion.Domain/Entities/ProductColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Domain.Entities
+{
+    public class ProductColor
+    {
+
+        //relational properties 
+        public int ProductId { get; set; }
+        public int ColorId { get; set; }
+        public Product Product { get; set; }
+        public Color Color { get; set; }
+
+    }
+}
